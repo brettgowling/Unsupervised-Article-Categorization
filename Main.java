f088d47d42f2b07b59075b04fa361a7e9a9988c1
Blake Silton <55601729+BSilton26@users.noreply.github.com>
2023-03-20 14:52:16 -0700

    Add capability to serialize models or read from serialized models.

--- Main.java
+++ Main.java
@@ -1,14 +1,26 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
 
-    /* Boolean to indicate whether to generate and serialize a new DocumentCollection for the specified csv file.
-     * Best practice is to serialize a new DocumentCollection the first time checking out the repo on a new machine.
+    /* Booleans to indicate whether to generate and serialize new objects or read from serialized files.
+     *  - SERIALIZE_DOCUMENTS - Best practice is to serialize a new one the first time checking out the repo on a new
+     *                          machine. Performance benefit is minimal for small datasets like we're using.
+     *  - SERIALIZE_MODELS    - Useful for saving generated models since they are generally unique by nature.
+     *                          Will serialize intermediate models from multiple iterations and then serialize the best
+     *                          model.
      */
-    private static final boolean SERIALIZE = false;
+    private static final boolean SERIALIZE_DOCUMENTS = false;
+    private static final boolean SERIALIZE_MODELS = true;
+
     // Serialized object path constants
-    private static final String PATH_SERIALIZED_TRAINING_ARTICLE_COLLECTION = "serialized/train_articles_coln.bin";
-    private static final String PATH_SERIALIZED_TESTING_ARTICLE_COLLECTION = "serialized/test_articles_coln.bin";
+    private static final String PATH_SERIALIZED_DIR = "serialized/";
+    private static final String PATH_SERIALIZED_TRAINING_ARTICLE_COLLECTION =
+            PATH_SERIALIZED_DIR + "train_articles_coln.bin";
+    private static final String PATH_SERIALIZED_TESTING_ARTICLE_COLLECTION =
+            PATH_SERIALIZED_DIR + "test_articles_coln.bin";
+    private static final String PATH_SERIALIZED_MODEL_FORMAT_STRING =
+            PATH_SERIALIZED_DIR + "model_output_%s.bin";
 
     // File paths to different sets of data
     private static final String PATH_TRAINING_DATA = "learn-ai-bbc/BBC News Train.csv";
@@ -32,7 +44,7 @@ public class Main {
 //        String serializedDocCollectionPath = PATH_SERIALIZED_TESTING_ARTICLE_COLLECTION;
 
         // Load DocumentCollection from memory to save time, or create one if specified or doesn't exist
-        if (SERIALIZE) {
+        if (SERIALIZE_DOCUMENTS) {  // Generate and serialize new DocumentCollections
             System.out.printf("Generating new DocumentCollection from: %s\n", dataPath);
             articles = new DocumentCollection(dataPath, "articles");
 
@@ -41,7 +53,7 @@ public class Main {
 
             System.out.printf("Serializing DocumentCollection to: %s\n", serializedDocCollectionPath);
             serialize(articles, serializedDocCollectionPath);
-        } else {
+        } else {  // Read DocumentCollection from serialized object
             System.out.printf("Reading DocumentCollection from %s\n", serializedDocCollectionPath);
             articles = (DocumentCollection) load(serializedDocCollectionPath);
         }
@@ -113,31 +125,55 @@ public class Main {
         //     System.out.println(string + ": " + articles.category_counts.get(string));
         // }
 
-        // uncomment to make it work
+        ArrayList<KMeanClusteringModel> models = new ArrayList<>();
+        if (SERIALIZE_MODELS) {  // Generate and serialize new models
+            for (int iteration = 0; iteration < (Math.log(articles.getSize()) / Math.log(2)); iteration++){
+                KMeanClusteringModel model = new KMeanClusteringModel(articles, 5);
+                model.fitCentroids();
+                models.add(model);
+                serialize(model, String.format(PATH_SERIALIZED_MODEL_FORMAT_STRING, iteration));
+
+                break; //FIXME: force single iteration, so we only generate one model
+            }
+        } else {  // Read models from serialized objects
+            File serializedDir = new File(PATH_SERIALIZED_DIR);
+            if (serializedDir.isDirectory()) {
+                // Get a list of all files that match model filename
+                File[] modelFiles = serializedDir.listFiles((d, name) -> name.matches("model_output_\\d\\.bin"));
+                assert modelFiles != null;
+                // Iterate through the models in order since File.listFiles doesn't guarantee order
+                for (int i = 0; i < modelFiles.length; i++) {
+                    // Load each model and store in our `models` ArrayList
+                    String thisModelFilename = String.format(PATH_SERIALIZED_MODEL_FORMAT_STRING, i);
+                    KMeanClusteringModel model = (KMeanClusteringModel) (load(thisModelFilename));
+                    models.add(model);
+                }
+            }
+        }
+
+        // After creating or loading models, we check their performance
         double max_precision = 0.0;
         double max_recall = 0.0;
         double max_f1_score = 0.0;
-
-        for (int iteration = 0; iteration < (Math.log(articles.getSize()) / Math.log(2)); iteration++){
-            KMeanClusteringModel model = new KMeanClusteringModel(articles, 5);
-            model.fitCentroids();
-
-            //TODO: Shouldn't we be using testing data for these calculations?
+        int bestModelIndex = -1;
+        for (KMeanClusteringModel model : models) {
+            //FIXME: Shouldn't we be using testing data for these calculations?
             double curr_max_precision = model.getWeightedPrecision(articles);
             double curr_max_recall = model.getWeightedRecall(articles);
             double curr_f1_score = get_f1_score(curr_max_precision, curr_max_recall);
 
-            if(curr_f1_score > max_f1_score) {
+            if (curr_f1_score > max_f1_score) {
                 max_precision = curr_max_precision;
                 max_recall = curr_max_recall;
                 max_f1_score = curr_f1_score;
+                bestModelIndex = models.indexOf(model);
             }
         }
 
+        System.out.println("Best Model: " + String.format(PATH_SERIALIZED_MODEL_FORMAT_STRING, bestModelIndex));
         System.out.println("FINAL MAX PRECISION: \t" + max_precision);
         System.out.println("FINAL MAX RECALL: \t" + max_recall);
         System.out.println("FINAL MAX F1-SCORE: \t" + max_f1_score);
-
 
         System.out.println("Ending...");
     }
